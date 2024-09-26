cbefc69d794b9d6a091bd4341dddeec08c1dc7ed
Haroon Younis <haroon.younis00@gmail.com>
2024-09-26 02:11:59 +0100

    first commit

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 // Create an Express app
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 // Use CORS
 app.use(cors());
